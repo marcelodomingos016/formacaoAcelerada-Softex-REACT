ce158df69888030c99a19d17b97974100f96865c
Marcelo Augusto <93164093+marcelodomingos016@users.noreply.github.com>
2023-01-24 17:46:10 -0300

    Add files via upload

---
+++ Atividade02/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Marcelo Augusto da Silva Domingos</h1>
+      <h2>Meu nome</h2>
+    </div>
+  );
+}
+
+export default App;
