c5f03f2730a74d5c36b59001ed0390c74694382b
Marcelo Augusto <93164093+marcelodomingos016@users.noreply.github.com>
2023-01-24 17:55:05 -0300

    Add files via upload

---
+++ Atividade04/src/App.js
@@ -0,0 +1,11 @@
+import Dados from './componentes/Dados'
+
+export default function App() {
+  return (
+    <>
+      <h1>Utilizando PROPS</h1>
+      <Dados nome=' Marcelo' idade= ' 29 anos' sexo=' Masculino' />
+    </>
+  );
+}
+
